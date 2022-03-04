94cd402e1bdd158caabfd5c452c183c7c55855ed
patrick ntwali <patrick_ntwari@yahoo.com>
2022-03-04 14:40:45 +0200

    PNT: Completed edit section for category model

--- Skillsbox.Challenge.MovieBooking.Web/Program.cs
+++ Skillsbox.Challenge.MovieBooking.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl")) });
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
